ae9496dcb0e4ebf1c4b0a9c9df9f1ae40b55fcf9
Reshad402 <mdrhr402@gmail.com>
2022-10-31 20:18:55 +0600

    Give unique key and send the props

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
 import './Shop.css'
 const Shop = () => {
     const [products, setProducts] = useState([])
@@ -11,7 +12,14 @@ const Shop = () => {
     return (
         <div className='shop-container'>
             <div className="products-container">
-                    <h3>This is for product: {products.length}</h3>
+                    {/* <h3>This is for product: {products.length}</h3> */}
+                    {
+                        products.map(product =><Product 
+                            sendProps ={product}
+                            key={product.key}>
+
+                            </Product>)
+                    }
             </div>
             <div className="cart-container">
                     <h4>This is for cart</h4>
